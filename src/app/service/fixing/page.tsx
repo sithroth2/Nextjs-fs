147cbbc65ed1cc10207c4b6d5a8a86940842b88d
ingdavann <ingdavann4444@gmail.com>
2025-06-25 22:41:33 +0700

    Init Project: Basic route, styling

---
+++ src/app/service/fixing/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      This is service page for fixing issues
+    </div> 
+    // /service/fixing/page.tsx = segment
+  )
+}
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './style.module.css';
+
+export default function Button() {
+  return (
+    <button className={styles.background}>
+      Login
+    </button>
+  )
+}
