c37d0333f3d4086ed585883da3a341e07aa3f42c
ingdavann <ingdavann4444@gmail.com>
2025-07-04 21:32:21 +0700

    added: data fetching on product

--- src/components/navbar/NavbarComponent.tsx
+++ src/components/navbar/NavbarComponent.tsx
@@ -3,25 +3,27 @@ import Link from 'next/link'
 import React from 'react'
 
 export default function NavbarComponent() {
-  return (
-    <nav className="w-[90%] mx-auto relative flex items-center justify-between sm:h-10 md:justify-center py-6 px-4 mt-2">
-        <div className="flex items-center flex-1">
-            <div className="flex items-center justify-between w-full md:w-auto">
-                <div className="font-bold text-xl text-gray-900">
-                   <Link href="/">MyLogo</Link>
+    return (
+        <nav className="w-[90%] mx-auto relative flex items-center justify-between sm:h-10 md:justify-center py-6 px-4 mt-2">
+            <div className="flex items-center flex-1">
+                <div className="flex items-center justify-between w-full md:w-auto">
+                    <div className="font-bold text-xl text-gray-900">
+                        <Link href="/">MyLogo</Link>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="hidden md:flex md:space-x-10">
-            <Link href="/service"
-                className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Service</Link>
-            <Link href="/about"
-                className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">About</Link>
-            <Link href="/blog"
-                className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Blog</Link>
-            
-        </div>
-      
-    </nav>
-  )
+            <div className="hidden md:flex md:space-x-10">
+                <Link href="/service"
+                    className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Service</Link>
+                <Link href="/about"
+                    className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">About</Link>
+                <Link href="/product"
+                    className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Product</Link>
+                <Link href="/blog"
+                    className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Blog</Link>
+
+            </div>
+
+        </nav>
+    )
 }
---
+++ src/types/product.ts
@@ -0,0 +1,9 @@
+export type Product = {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    category: string;
+    thumbnail: string;
+}
